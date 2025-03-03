8922e75469fc2dffee4b7af609a7af243c6fef09
Michael Fuchs <m-fuchs>
2025-03-03 08:53:26 +0100

    * add project base * add logo file * add documentation

---
+++ forms/movie2script.forms.mainform.pas
@@ -0,0 +1,43 @@
+(* Movie2Script
+
+   Copyright (C) 2025 Michael Fuchs,  https://www.ypa-software.de
+
+   This source is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+   version.
+
+   This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   A copy of the GNU General Public License is available on the World Wide Web at
+   <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing to the Free Software Foundation, Inc., 51
+   Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.
+*)
+
+unit Movie2Script.Forms.MainForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
